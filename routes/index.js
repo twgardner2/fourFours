ef4cd91db07e608c43b434dddf5432e772b462a6
Tom Gardner <twgardner2@gmail.com>
2018-07-15 10:41:19 -0400

    restructuring as a simple website for test deployment

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router(),
-      homeRoutes = require('./homeRoutes.js'),
-      foodRoutes = require('./foodRoutes.js');
-
-router.use('/food', foodRoutes)
-      .use('/', homeRoutes);
-
-module.exports = router;
